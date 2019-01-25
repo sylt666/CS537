61f817cc08ff77ad48e1d81d7d6ba653a2b67254
Schmidt <gubba@Schmidts-MacBook-Air.local>
2019-01-25 14:31:07 -0600

    my-sed v1.1

--- P1/p1a/my-sed.c
+++ P1/p1a/my-sed.c
@@ -9,30 +9,37 @@ int main(int argc, char *argv[]) {
 	if (argc == 0) {
 		exit(1);
 	}
-	else if(argc == 1) {
+	else if (argc == 1) {
 		exit(0);
 	}	
-    else if(argc == 2) {
+    else if (argc <= 2 || argc > 3) {
 		printf("my-sed: find_term replace_term [file …]\n");
         exit(0);
 	}
 	else {
 		for (int i = 1; i < argc; i++) {
-			FILE *fp = fopen(argv[i], "r");
-			if(fp == NULL) {
-				printf("my-sed: cannot open file\n");
-				exit(1);
-			} else {
-				char buffer[BUFFER_SIZE];
-                char* line = NULL;
-                size_t length = 0;
-                ssize_t read = 0;
-                while((read = getLine(&line, &length, fp))){
-                    if(strstr(line, argv[1] != NULL)) {
-                        printf("%s", line);
+            char* find_term = argv[1];
+            char* replace_term = argv[2];
+            if (find_term == NULL || replace_term == NULL) {
+
+            }
+            for (int i = 3; i < argc; i++) {
+			    FILE *fp = fopen(argv[i], "r");
+			    if (fp == NULL) {
+				    printf("my-sed: cannot open file\n");
+				    exit(1);
+			    } else {
+				    char buffer[BUFFER_SIZE];
+                    char* line = NULL;
+                    size_t length = 0;
+                    ssize_t read = 0;
+                    while((read = getLine(&line, &length, fp))){
+                        if (strstr(line, argv[1] != NULL)) {
+                            printf("%s", line);
+                        }
                     }
-                }
-			}
+			    }
+            }
 		}	
 	}
 	return 0;
