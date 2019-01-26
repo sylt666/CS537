5e131f5464f94ac339e10d521db5329b739bbb18
Schmidt <gubba@Schmidts-MacBook-Air.local>
2019-01-25 19:57:25 -0600

    my-sed v2

--- P1/p1a/my-sed.c
+++ P1/p1a/my-sed.c
@@ -2,7 +2,45 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define BUFFER_SIZE (512)
+char *replaceWord(const char *str, const char *old, const char *new) {
+    char *result; 
+    int i, cnt = 0; 
+    int newWlen = strlen(new); 
+    int oldWlen = strlen(old); 
+  
+    // Counting the number of times old word 
+    // occur in the string 
+    for (i = 0; str[i] != '\0'; i++) 
+    { 
+        if (strstr(&str[i], old) == &str[i]) 
+        { 
+            cnt++; 
+  
+            // Jumping to index after the old word. 
+            i += oldWlen - 1; 
+        } 
+    } 
+  
+    // Making new string of enough length 
+    result = (char *)malloc(i + cnt * (newWlen - oldWlen) + 1); 
+  
+    i = 0; 
+    while (*str) 
+    { 
+        // compare the substring with the result 
+        if (strstr(str, old) == str) 
+        { 
+            strcpy(&result[i], new); 
+            i += newWlen; 
+            str += oldWlen; 
+        } 
+        else
+            result[i++] = *str++; 
+    } 
+  
+    result[i] = '\0'; 
+    return result; 
+}
 
 int main(int argc, char *argv[]) {
 	
@@ -12,27 +50,32 @@ int main(int argc, char *argv[]) {
 	else if (argc == 1) {
 		exit(0);
 	}	
-    else if (argc < 4 || argc > 4) {
+    else if (argc < 4) {
 		printf("my-sed: find_term replace_term [file …]\n");
         exit(0);
 	}
 	else {
-            char* find_term = argv[1];
-            char* replace_term = argv[2];
             for (int i = 3; i < argc; i++) {
 			    FILE *fp = fopen(argv[i], "r");
 			    if (fp == NULL) {
 				    printf("my-sed: cannot open file\n");
 				    exit(1);
 			    } else {
-				    //char buffer[BUFFER_SIZE];
-                    printf("looking for %s and replacing it with %s in the file %s", find_term, replace_term, argv[3]);
+                    char* find_term = argv[1];
+                    char* replace_term = argv[2];
+
                     char* line = NULL;
                     size_t length = 0;
                     ssize_t read = 0;
-                    while((read = getline(&line, &length, fp))){
-                        if (strstr(line, argv[i]) != NULL) {
-                            printf("%s", line);
+                    while ((read = getline(&line, &length, fp) != -1)) {
+                        if (strstr(line, argv[1]) != NULL) {
+                            char *result = NULL; 
+
+                            //printf("Old string: %s\n", line); 
+                            result = replaceWord(line, find_term, replace_term); 
+                            //printf("New String: %s\n", result); 
+                            free(result);                          
+                            //printf("%s", line);
                         }
                     }
                     fclose(fp);
--- P1/p1a/my-uniq.c
+++ P1/p1a/my-uniq.c
@@ -7,17 +7,39 @@ int main(int argc, char *argv[]) {
     // ~cs537-1/tests/p1a/test-my-cat.csh
     // ~cs537-1/tests/p1a/test-my-cat.csh -v
     // ./my-cat [file]
-
-    FILE *f = fopen(argv[1], "r");
-
-    // if error exit
-    if (f == NULL) {
-        printf("file %s not found\n", argv[1]);
-        exit(1);
-    }
-    
-    // Passing NULL into fclose() causes segmentation fault when file does not exist
-    fclose(f);
-
-    return 0;
+	
+	if (argc == 0) {
+		exit(1);
+	}
+	else if (argc == 1) {
+		exit(0);
+	}	
+    else if (argc < 4 || argc > 4) {
+		printf("my-sed: find_term replace_term [file …]\n");
+        exit(0);
+	}
+	else {
+            for (int i = 3; i < argc; i++) {
+			    FILE *fp = fopen(argv[i], "r");
+			    if (fp == NULL) {
+				    printf("my-sed: cannot open file\n");
+				    exit(1);
+			    } else {
+				    //char buffer[BUFFER_SIZE];
+                    char* find_term = argv[1];
+                    char* replace_term = argv[2];
+                    printf("looking for %s, replaceing with %s, looking in file: %s\n",find_term, replace_term, argv[3]);
+                    char* line = NULL;
+                    size_t length = 0;
+                    ssize_t read = 0;
+                    while ((read = getline(&line, &length, fp) != -1)) {
+                        if (strstr(line, argv[1]) != NULL) {
+                            printf("%s", line);
+                        }
+                    }
+                    fclose(fp);
+			    }
+            }
+		}	
+	return 0;
 }
