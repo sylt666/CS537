1b1a59f307bdefb05bc792cd7f1100f3ce4a160e
Schmidt <gubba@Schmidts-MacBook-Air.local>
2019-02-10 11:59:29 -0600

    testing

--- p2a-copy/wish.c
+++ p2a-copy/wish.c
@@ -1,158 +1,222 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <string.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <string.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
 
-#define MAX_LENGTH 1024
-#define PIPE_LENGTH 1
 
-int main (int argc, char *argv[]) {
-	while(1) {
-		printf("mysh> ");
-		// line stores the contents from the commnad line. 
-			
-		char line [MAX_LENGTH];
-		// "char array" , after read from stdin, 'enter' -> \n, and '\0'automatically append to the end of the input, 
-		assert(fgets(line, MAX_LENGTH, stdin) != NULL);
-		//char *ptr;
-		//if ((ptr = strchr(line, '\n')) != NULL) {
-		//	*ptr = '\0';
-		//}
-		// the code below tries to parase the c string 'line' to an array
-		int i;
-		int status = -1; // $status = -1 wait for any child return
-		int ret = -1; // return value for pipe
-		int fd[2] = {0}; // ? int array ?
-		char* token, *str; // why *token
-		char* saveptr;
-		char* argv2 [MAX_LENGTH];
-		int redirect = 0; // 0 -> no, 1 -> yes
-		int go_pipe = 0; // 0 -> no pipe, 1 -> go pipe  
-		int redirName; // index of array that points to the name of the file to be stored
-		int truncate = 0; // 1 -> when truncate
-		int append = 0; // 1 -> when append
-		int pipe_index = 0; // create a char ptr array to store the index of the '|' sign. 
-		for (i = 0, str=line; ;i++, str= NULL) {
-			token = strtok_r(str, " \n", &saveptr); // " " - the second para. indicates the content to used to parase the str, "/ " - finds "/" and " " and sepearte do t			the parase. 
-			argv2[i] = token; //argv2 stores the content of the paraseed content. 
-			if (token == NULL)
-				break;
-			// check overwrite direction
-			if (strcmp(token, ">") == 0) {
-				redirect = 1;
-				redirName = i + 1; // get index of file name  after '>'
-				truncate = 1; // set truncate bit
+char error_message[30] = "An error has occurred\n";
+char *newenv[50]; 
+char *envpath = "/bin";
+int noofprocesses = 0;
+char *builtin[] = {"exit","cd","path"};
+
+
+void parallelrun(char *newargv[],int newargc, char * fileName){
+	int p = 0, fd= 0;
+	for(p=0;p<noofprocesses;p++){
+		char *fullname = malloc(strlen(newenv[p])+strlen(newargv[0])+1); //+1 for the null-terminator
+		fullname[0] = '\0';
+	    strcpy(fullname, newenv[p]);
+	    strcat(fullname, "/");
+	    strcat(fullname, newargv[0]);	
+		if(access(fullname,X_OK)==0){
+			int rc = fork();
+			if(rc==0){	
+			    newargv[newargc] = NULL;    
+			    if(fileName!=NULL){
+			    	// mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+					fd = open(fileName, O_RDWR|O_CREAT|O_TRUNC, 0600);
+					dup2(fd, 1);
+					close(fd);
+				}
+				execv(fullname, newargv);
+				dup2(2, 1);
+				exit(0);						
 			}
-			if (strcmp(token, ">>") == 0) {
-                                redirect = 1;
-                                redirName = i + 1; // get index of file name after '>>'
-                                append = 1; // set append bit
-                        }
-			// check pipe
-			if (strcmp(token, "|") == 0) {
-                                go_pipe = 1;
-                                pipe_index = i; // get pipe sign index
-                        }
-
-			
+			break;	
 		}
-
-		// if command line is empty
-		if (argv2[0] == NULL) {
-                	continue;
+		else{
+			if(p==noofprocesses-1){
+				write(STDERR_FILENO, error_message, strlen(error_message));
+			}
 		}
-		// if command 'exit', quit mysh
-		if (strcmp(argv2[0], "exit") == 0) {
-			// if the exit follow with args, return Error!
-			if (argv2[1] != NULL){
-				fprintf(stderr, "Error!\n");
+	}
+}
+
+void forRedirection(char *currargs[],int newargc){
+	if(newargc==0)
+		return;
+	int i = 0;
+	char *newargv[50];		
+	while(i<newargc && strcmp(currargs[i],">")!=0){
+		newargv[i] = currargs[i];
+		i++;
+	}
+	newargv[i] = NULL;
+	if(i==newargc){
+		parallelrun(newargv,newargc, NULL);
+		return;
+	}
+
+	if(i==newargc-1 || i<newargc-2){
+		write(STDERR_FILENO, error_message, strlen(error_message));
+		return;
+	}
+
+	if(i==0){
+		write(STDERR_FILENO, error_message, strlen(error_message));
+		return;
+	}
+	
+	parallelrun(newargv,newargc-2, currargs[newargc-1]);
+	return;
+}
+
+
+void execfn(char *inputline){
+	int i = 0;
+	char *newargv[256  * sizeof(char)];
+	int newargc = 0;	
+	while(i<strlen(inputline) && inputline[i]!='\n'){
+		if(inputline[i]!= ' ' && inputline[i]!='\t' ){
+			if(inputline[i]=='&'){
+				char *temparg = malloc(256  * sizeof(char));	
+				temparg[0] = '&';
+				temparg[1] = '\0';
+				newargv[newargc] = temparg;
+				newargc++;
+				i++;
 				continue;
 			}
-			// if the command line with 'exit' is not follow by any args, exit.
-			exit(0);
-		}
-		 
-		if (strcmp(argv2[0], "cd") == 0) {
-			if (argv2[1] == NULL) {
-				if(chdir(getenv("HOME")) == -1)
-					fprintf(stderr, "Error!\n");
-			} else {
-				if(chdir(argv2[1]) == -1) {
-					fprintf(stderr, "Error!\n");
-				}
-			}
-			continue;	
-		}
-		// pwd 
-		if (strcmp(argv2[0], "pwd") == 0) {
-                        char cwd [1024];
-			assert(getcwd(cwd, 1024) != NULL);
-			printf("%s\n", cwd);
-			continue;
-                }
 
-		// fork the child process to run the require task. 
-		int rv; 
-		rv = fork();
-		if (rv == 0) {
-			// printf("Child\n");
-			if (argv2[0] == NULL) {
-				break;
+			if(inputline[i] == '>'){
+				char *temparg = malloc(256  * sizeof(char));	
+				temparg[0] = '>';
+				temparg[1] = '\0';
+				newargv[newargc] = temparg;
+				newargc++;
+				i++;
+				continue;
 			}
-			if (redirect) {
-				close(1); // close STDOUT
-				int fd; 
-				if (truncate) {
-					fd = open(argv2[redirName], O_CREAT|O_WRONLY|O_TRUNC, 0666);
-					argv2[redirName - 1] = NULL; // reset the NULL pointer for the argv2
-					// printf("redirName: %s, %s\n", argv2[redirName], argv2[redirName + 1]);
+
+			int t = 0;	
+			char *temparg = malloc(256  * sizeof(char));
+			while(i<strlen(inputline) && inputline[i]!=' ' && inputline[i] != '\t' && inputline[i]!='\n' && inputline[i]!='\r'){
+				if(inputline[i]!= '&' && inputline[i]!='>'){					
+				temparg[t] = inputline[i];
+				i++;
+				t++;
 				}
-				if (append) {
-					fd = open(argv2[redirName], O_CREAT|O_WRONLY|O_APPEND, 0666);
-                			argv2[redirName - 1] = NULL; // reset the NULL pointer for the argv2
-				}
+				else break;
 			}
-				
-			if (go_pipe) {
-				ret = pipe(fd);
-				if (ret == -1) {
-					perror("Pipe failed!\n");
-					// goto _ERR;
-				}
-				rv = fork(); // grandchild
-				if (rv == 0) {
-					// grandchild process
-					close(fd[0]); // close read pipe
-					dup2(fd[1], 1); // cp fd[1] as the STDOUT
-					argv2[pipe_index] = NULL;
-					if (execvp(argv2[0], argv2) == -1)
-						perror("Error!\n");
-				}
-				if (rv > 0) {
-					// child process
-					wait(&status); // child wait for grandchild
-					close(fd[1]); // close write pipe
-					dup2(fd[0], 0); // cp fd[0] as the STDIN
-					//argv2 = argv2[pipe_index + 1];
-					//argv2+(pipe_index+1)
-					if (execvp((argv2+(pipe_index+1))[0], argv2+(pipe_index+1)) == -1) 
-						perror("ERROR!\n");
-				}
-			}
-					
-	
-            
-			// use child process to run the require command. 
-			if(execvp(argv2[0], argv2) == -1)
-				perror("Error!\n");
-		} else if (rv > 0) {
-			// printf("parent\n");
-			wait(&status); // parent wait for child
-		}		
+			temparg[t] = '\0';
+			newargv[newargc] = temparg;
+			newargc++;
+		}
+		else{
+			i++;
+		}
 	}
-	return 0;		
+	
+	if(newargc==0) return;
+	if(strcmp(newargv[0],builtin[0])==0){ 
+		if(newargc==1)
+			exit(0);
+		else	
+			write(STDERR_FILENO, error_message, strlen(error_message));
+	}
+	
+	else if(strcmp(newargv[0],builtin[1])==0){
+		if(newargc!= 2){
+			write(STDERR_FILENO, error_message, strlen(error_message));
+			return;
+		}
+		else{
+			if(chdir(newargv[1])!=0){
+				write(STDERR_FILENO, error_message, strlen(error_message));	
+			} 
+		}
+	}
+	else if(strcmp(newargv[0],builtin[2])==0){ // overwrite env variable
+		noofprocesses = newargc - 1;
+		int j;
+		for(j=0;j<newargc -1;j++){
+			newenv[j] = newargv[j+1];
+		}
+	}
+	else{	
+		if(noofprocesses==0){
+			write(STDERR_FILENO, error_message, strlen(error_message));
+			return;
+		}
+
+		int i = 0;
+		int numCommand = 0;
+		while(i<newargc){
+			char *currargs[50];
+			int j = 0;
+			while(i<newargc && strcmp(newargv[i],"&")!=0){
+				currargs[j] = newargv[i];
+				i++;
+				j++;
+			}
+			numCommand++;
+			currargs[j] = NULL;
+			forRedirection(currargs, j);
+			i++;
+		}
+		while(numCommand-- >0)
+			wait(NULL);	
+	}
+
+}
+
+
+void batch(char *fileName){
+	FILE *fp = fopen(fileName, "r");
+	size_t length=0;
+	if(fp==NULL){
+		write(STDERR_FILENO, error_message, strlen(error_message));
+		exit(1);
+	}
+	char* line = NULL;
+	while (getline(&line, &length, fp)!=-1) {
+		execfn(line);
+	}
+}
+
+
+
+void user(){
+	char *line = NULL;
+	size_t length = 0;
+	while(1){
+		printf("wish> ");
+		fflush(stdout);
+		if(getline(&line, &length, stdin)!=-1)
+			execfn(line);
+		else break;
+	}
+}
+
+int main(int argc, char *argv[]){	
+	newenv[0] = envpath;
+	noofprocesses = 1;	
+
+	if(argc==1){
+		user();
+	}
+	else if(argc==2){
+		batch(argv[1]);
+	}
+	else{
+		write(STDERR_FILENO, error_message, strlen(error_message));
+		exit(1);
+	}
+	return 0;
 }
